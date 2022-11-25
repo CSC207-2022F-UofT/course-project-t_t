65ee4494742c2e883ec2e3ce293c821fb2603cdf
Honours-Arts-and-Business <113327053+Honours-Arts-and-Business@users.noreply.github.com>
2022-11-25 16:41:58 -0500

    Add files via upload
    added back useCases from old repo

---
+++ src/main/java/useCases/SetLocation.java
@@ -0,0 +1,27 @@
+package useCases;
+import entities.Student;
+import entities.Location;
+import entities.Timetable;
+import entities.Section;
+import entities.Interval;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+public class SetLocation{
+    public void manualSetLocation(Student student, Location location){
+        student.setLocation(location);
+    }
+
+    public void automaticSetLocation(Student student){
+        LocalDateTime now = LocalDateTime.now().minusHours(5);
+        int now_m = 24 * 60 * (now.getDayOfWeek().getValue() - 1) + 60 * now.getHour() + now.getMinute();
+        Timetable tt = student.getTimetable();
+        ArrayList<Section> sections = tt.getSections();
+
+        for(int i = 0; i < sections.size(); i++){
+            Interval interval = sections.get(i).intervals();
+        }
+    }
+}
+

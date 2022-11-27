4b8f1e65b7ce8d73afe3ccba6a7ff35889877d15
Clarence <clarence.comsci@gmail.com>
2022-11-27 18:59:54 -0500

    changed database

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,17 +1,27 @@
 package database;
+
 import entities.User;
 import entities.Timetable;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 import java.util.ArrayList;
 
 public class Database {
     private static ArrayList<User> db = new ArrayList<User>();
 
-    public getDatabase() {
+    public ArrayList<User>getDatabase() {
 
     }
 
-    public setDatabase(ArrayList<User>) {
-        
+    public void setDatabase(ArrayList<User> l) {
+
     }
 }

a341caff3c73b8de6b1fbfc3a25962f7493549fd
naraelee <nar.lee@mail.utoronto.ca>
2022-11-28 22:35:35 -0500

    debugging timetableviewer

--- src/main/java/presenter/TimetableViewer.java
+++ src/main/java/presenter/TimetableViewer.java
@@ -4,21 +4,34 @@ import entities.Course;
 import entities.Timetable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TimetableViewer {
     public ArrayList<String> visualizeTimetable(Timetable timetable) {
 
         ArrayList<Course> courses = timetable.getCourses();
-        ArrayList<String> schedule = new ArrayList<String>();
+        ArrayList<String> schedule = new ArrayList<>();
 
         StringBuilder sch = new StringBuilder();
 
-        for (Course cou: courses) {
-            sch.append('\n' + '[' + cou.getCourseCode() + '\n' );
+        for (Course cou : courses) {
+            sch.append('\n' + '[' + cou.getCourseCode() + '\n');
             sch.append(cou.getSectionCode() + '\n');
-            sch.append( + ']');
+            sch.append(+']');
+
+            schedule.add(sch.toString());
 
         }
+
+        System.out.println("Test");
+
         return schedule;
+
+
+
+
+
+
+
     }
 }

59337ddefef29ca1c44df0ed049679f7d7b43a3c
Clarence <clarence.comsci@gmail.com>
2022-11-30 18:24:44 -0500

    build.gradle method

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,57 +1,46 @@
 package database;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+import com.github.dozermapper.core.DozerBeanMapper;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import entities.User;
 import entities.Timetable;
 import entities.Location;
 import entities.FriendsList;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
 import java.io.*;
 
-import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import javax.swing.text.html.parser.Parser;
-import javax.xml.xpath.XPathEvaluationResult;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
-public class Database {
+public class Database{
 
-    public static void main(String[] args) {
-
-    }
+//    public static void main(String[] args) {
+//        Database db = new Database();
+//        db.getDatabase();
+//    }
 
     private static final ArrayList<User> db = new ArrayList<User>();
 
-    public ArrayList<User> getDatabase() {
-        JSONParser parser = new JSONParser();
-        ArrayList<User> returnArrayList = new ArrayList<User>();
-        try{
-            JSONArray a = (JSONArray) parser.parse(new FileReader("UserDataBase.json"));
-            for (Object o: a) {
-                JSONObject user = (JSONObject) o;
-                String username = (String) user.get("username");
-                String password = (String) user.get("password");
-                String email = (String) user.get("email");
-                Location location = (Location) user.get("location");
-                Timetable timetable = (Timetable) user.get("timetable");
-                FriendsList friendsList = (FriendsList) (user.get("FriendsList"));
-                User newUser = new User(username, password, email, location, timetable, friendsList);
-                returnArrayList.add(newUser);
-            }
-            return returnArrayList;
-        } catch(FileNotFoundException e) {
+    public void getDatabase() {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("UsersDataBase.json"));
+            JsonObject jsonObject = (JsonObject) Jsoner.deserialize(reader);
+            Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+            User user = mapper.map(jsonObject, User.class);
+            System.out.println(user);
+            reader.close();
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("FileNotFoundException");
-        } catch(IOException e) {
-            e.printStackTrace();
-            System.out.println("IOException");
-        } catch(ParseException e) {
-            e.printStackTrace();
-            System.out.println("ParseException");
         }
-        return returnArrayList;
     }
 }
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,13 +1,19 @@
 package entities;
 
-public class User {
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsonable;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class User implements Jsonable {
 
     private String username;
     private String password;
     private String email;
-    private Timetable timetable;
     private FriendsList friendsList;
     private Location location;
+    private Timetable timetable;
 
     public User(String username, String password, String email, Location location,
                 Timetable timetable, FriendsList friendsList) {
@@ -57,4 +63,25 @@ public class User {
     public void setTimetable(Timetable timetable) {this.timetable = timetable;}
 
     public void setLocation(Location location) {this.location = location;}
+
+    @Override
+    public String toJson() {
+        JsonObject json = new JsonObject();
+        json.put("username", this.username);
+        json.put("password", this.password);
+        json.put("email", this.email);
+        json.put("friendsList", this.friendsList);
+        json.put("location", this.location);
+        json.put("timetable", this.timetable);
+        return json.toJson();
+    }
+
+    @Override
+    public void toJson(Writer writable) throws IOException {
+        try {
+            writable.write(this.toJson());
+        } catch (Exception ignored) {
+
+        }
+    }
 }

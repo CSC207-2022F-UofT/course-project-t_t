cae91df156346c90914a8a71d71da93c5d39f4dd
great-prophet <discussion.abstract@gmail.com>
2022-12-01 16:43:44 -0500

    course comparer class

---
+++ src/main/java/pages/TimetablePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class TimetablePage extends Page{
+    public TimetablePage(Router router, PageState pageState) {
+        super(router, pageState, "Timetable Page");
+    }
+
+    @Override
+    public Page run() {
+        return null;
+    }
+}

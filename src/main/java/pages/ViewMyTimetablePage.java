697c433402870ddfcb66da1c82d5038122a99248
naraelee <nar.lee@mail.utoronto.ca>
2022-12-01 19:32:04 -0500

    timetableviewer draft

--- src/main/java/pages/ViewMyTimetablePage.java
+++ src/main/java/pages/ViewMyTimetablePage.java
@@ -4,43 +4,63 @@ import entities.Timetable;
 import presenter.TimetableViewer;
 
 import javax.swing.*;
-import java.util.ArrayList;
+import java.util.Map;
+
 
 public class ViewMyTimetablePage extends Page{
+    // User curr_user = this.pageState.getCurrentUser();
+    public ViewMyTimetablePage(Router router, PageState pageState) {
+        super(router, pageState, "View My Timetable Page");
+    }
     public Timetable getTimetable() {
         return timetable;
     }
-    // if student1 and student2 both added each other to their friend lists,
-    // then student1 can view student2's timetable and student2 can view student1's timetable
-    // else if only student1 added student2 to its friend list,
-    // then student1 cannot view student2's timetable
-
     public void setTimetable(Timetable timetable) {
         this.timetable = timetable;
     }
 
-    private Timetable timetable;
-    private TimetableViewer timetableViewer;
-
-    public ViewMyTimetablePage(Router router, PageState pageState) {
-        super(router, pageState, "Timetable Viewer");
-        this.timetableViewer = new TimetableViewer();
+    private enum Option {
+        RETURN_TO_HOME_PAGE
     }
 
+    private Timetable timetable;
+    private TimetableViewer timetableViewer;
     @Override
     public Page run() {
-        ArrayList<String> visualization = this.timetableViewer.visualizeTimetable(this.timetable);
 
-        // show visualization
-        // prompt user to continue
+        System.out.println("This is my timetable:");
 
-        String[][] schedule = new String[5][22];
+        String[][] schedule = new String[23][6];
 
-        String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri"};
+        String[] days = {"Time", "Mon", "Tue", "Wed", "Thu", "Fri"};
+
+        schedule[0][0] = "9:00-9:30";
+        schedule[1][0] = "9:30-10:00";
+        schedule[2][0] = "10:00-10:30";
+        schedule[3][0] = "10:30-11:00";
+        schedule[4][0] = "11:00-11:30";
+        schedule[5][0] = "11:30-12:00";
+        schedule[6][0] = "12:00-12:30";
+        schedule[7][0] = "12:30-13:00";
+        schedule[8][0] = "13:30-14:00";
+        schedule[9][0] = "14:00-14:30";
+        schedule[10][0] = "14:30-15:00";
+        schedule[11][0] = "15:00-15:30";
+        schedule[12][0] = "15:30-16:00";
+        schedule[13][0] = "16:00-16:30";
+        schedule[14][0] = "16:30-17:00";
+        schedule[15][0] = "17:00-17:30";
+        schedule[16][0] = "17:30-18:00";
+        schedule[17][0] = "18:00-18:30";
+        schedule[18][0] = "18:30-19:00";
+        schedule[19][0] = "19:00-19:30";
+        schedule[20][0] = "19:30-20:00";
+        schedule[21][0] = "20:00-20:30";
+        schedule[22][0] = "20:30-21:00";
 
         JTable table = new JTable(schedule, days);
 
-        JFrame frame = new JFrame("Table demo");
+        JFrame frame = new JFrame("UTimetable");
 
         frame.add(new JScrollPane(table));
 
@@ -49,6 +69,19 @@ public class ViewMyTimetablePage extends Page{
 
         frame.setVisible(true);
 
-        return this.router.getHomePage();
+        Map<String, Option> options = Map.of(
+                "Return to Home Page", Option.RETURN_TO_HOME_PAGE
+        );
+        Option selection = promptInput(options);
+
+        Page redirect = null;
+
+        switch (selection) {
+            case RETURN_TO_HOME_PAGE:
+                redirect = this.router.getHomePage();
+                System.out.println("Redirecting to Home Page...");
+                break;
+        }
+        return redirect;
     }
 }
--- src/main/java/presenter/TimetableViewer.java
+++ src/main/java/presenter/TimetableViewer.java
@@ -3,47 +3,22 @@ package presenter;
 import entities.Course;
 import entities.Timetable;
 
-import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class TimetableViewer {
     public ArrayList<String> visualizeTimetable(Timetable timetable) {
 
         ArrayList<Course> courses = timetable.getCourses();
-        ArrayList<String> schedule = new ArrayList<>();
+        ArrayList<String> schedule = new ArrayList<String>();
 
         StringBuilder sch = new StringBuilder();
 
-        for (Course cou : courses) {
-            sch.append('\n' + '[' + cou.getCourseCode() + '\n');
+        for (Course cou: courses) {
+            sch.append('\n' + '[' + cou.getCourseCode() + '\n' );
             sch.append(cou.getSectionCode() + '\n');
-            sch.append(+']');
-
-            schedule.add(sch.toString());
+            sch.append( + ']');
 
         }
-
         return schedule;
-
-//        String[][] tab = new String[5][22];
-//
-//        String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri"};
-//
-//        JTable table = new JTable(tab, days);
-//
-//        JFrame frame = new JFrame("Table demo");
-//
-//        frame.add(new JScrollPane(table));
-//
-//        frame.setSize(500, 500);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        frame.setVisible(true);
-//
-//        System.out.println("Hello World!");
-
-
-
     }
 }

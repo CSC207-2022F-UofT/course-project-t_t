8e19460654d0b7dd730b7a2c5c8351d895a161a2
naraelee <nar.lee@mail.utoronto.ca>
2022-12-02 02:49:09 -0500

    timetableviewer semi final

--- src/main/java/pages/FreeIntervalPage.java
+++ src/main/java/pages/FreeIntervalPage.java
@@ -38,14 +38,14 @@ public class FreeIntervalPage extends Page{
             start += 1;
             end += 1;
         }
-
-        int startInd = freeIntervals.get(0).getStart();
-        int endInd = freeIntervals.get(0).getEnd();
-        int hour = (startInd - 1440) / 60;
-
         for (Interval inter: freeIntervals) { // selecting the index j
+            int startInd = inter.getStart();
+            int endInd = inter.getEnd();
+
             if (1440 <= startInd & startInd < 2880) { // monday
+
                 int day = 1;
+                int hour = (startInd - 1440) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
@@ -59,6 +59,7 @@ public class FreeIntervalPage extends Page{
             }
             else if (2880 <= startInd & startInd < 4320) { // tuesday
                 int day = 2;
+                int hour = (startInd - 2880) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
@@ -72,6 +73,7 @@ public class FreeIntervalPage extends Page{
             }
             else if (4320 <= startInd & startInd < 5760) { // wednesday
                 int day = 3;
+                int hour = (startInd - 4320) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
@@ -83,72 +85,64 @@ public class FreeIntervalPage extends Page{
                     schedule[hour][day] = "Free";
                 }
             }
-
             else if (5760 <= startInd & startInd < 7200) { //thursday
                 int day = 4;
+                int hour = (startInd - 5760) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
 
                     schedule[pmHour][day] = "Free";
                 }
-
                 else {
                     schedule[hour][day] = "Free";
                 }
             }
-
             else if (7200 <= startInd & startInd < 8640) { //friday
                 int day = 5;
+                int hour = (startInd - 7200) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
 
                     schedule[pmHour][day] = "Free";
                 }
-
                 else {
                     schedule[hour][day] = "Free";
                 }
             }
-
             else if (8640 <= startInd & startInd < 10080) { // saturday
                 int day = 6;
+                int hour = (startInd - 8640) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
 
                     schedule[pmHour][day] = "Free";
                 }
-
                 else {
                     schedule[hour][day] = "Free";
                 }
             }
-
             else if (10080 <= startInd & startInd < 11520) { // sunday
                 int day = 7;
+                int hour = (startInd - 10080) / 60;
 
                 if (hour > 12) {
                     int pmHour = hour - 12;
-
                     schedule[pmHour][day] = "Free";
                 }
-
                 else {
                     schedule[hour][day] = "Free";
                 }
             }
-
         }
-
         JTable table = new JTable(schedule, days); // visualizing into JTable
         JFrame frame = new JFrame("Free Interval Finder");
         frame.add(new JScrollPane(table));
-        frame.setSize(800, 500);
+        frame.setSize(1000, 250);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
 
         return router.getTimetablePage();
     }

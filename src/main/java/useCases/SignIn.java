7cc7a0ef2538348d90f38d3088474a90bc591d97
daybigh <darrenkim2036@gmail.com>
2022-12-07 23:23:10 -0500

    misc changes

--- src/main/java/useCases/SignIn.java
+++ src/main/java/useCases/SignIn.java
@@ -11,7 +11,6 @@ public class SignIn {
      * Iterates over the list of existing users and
      * checks if the given username matches any of the usernames in the database.
      *
-     *
      * @param username1 username given by the user.
      *
      * @return True if the given username is in the database. False otherwise.
@@ -19,7 +18,7 @@ public class SignIn {
     public boolean checkUsername(String username1) {
         return DatabaseGateway.contains(username1);
     }
-    
+
     /**
      * Uses a loop to find the index of the user with the given username in the database and if the user is found,
      * the method checks if the given password matches the password of the user with the given username.
@@ -34,9 +33,6 @@ public class SignIn {
      */
     public boolean checkPassword(String username2, String password1) {
         return Objects.equals(DatabaseGateway.getUser(username2).getPassword(), password1);
-
-
-
     }
 
 }
